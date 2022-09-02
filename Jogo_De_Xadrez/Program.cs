5af276b37767d0a5dc98408893fe1040b127cae6
Leandro dos Santos <le_yalm@hotmail.com>
2022-09-02 03:06:15 -0300

    Projeto iniciado

---
+++ Jogo_De_Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jogo_De_Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
