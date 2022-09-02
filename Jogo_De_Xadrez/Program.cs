05e1677349c8ed92e27ad68fac3757f4a44236a0
Leandro dos Santos <le_yalm@hotmail.com>
2022-09-02 04:21:20 -0300

    Melhorando a impressão do tabuleiro

--- Jogo_De_Xadrez/Program.cs
+++ Jogo_De_Xadrez/Program.cs
@@ -7,23 +7,25 @@ namespace Jogo_De_Xadrez
     {
         static void Main(string[] args)
         {
-            /* try
+             try
              {
                  Tabuleiro tab = new Tabuleiro(8, 8);
                  tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                  tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-                 Tela.imprimirTabuleiro(tab);
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.imprimirTabuleiro(tab);
              }
              catch (TabuleiroException e)
              {
                  Console.WriteLine(e.Message);
              }
-            */
+            
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            /*PosicaoXadrez pos = new PosicaoXadrez('c', 7);
             Console.WriteLine(  pos);
             Console.WriteLine(pos.ToPosicao());
+            */
             
 
         }
